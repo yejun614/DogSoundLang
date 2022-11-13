ac69e6c59f77532a50370532895e6861425d8de8
YeJun, Jung <31740224+yejun614@users.noreply.github.com>
2022-11-13 00:48:44 +0000

    Backup 6

--- src/includes/repl.h
+++ src/includes/repl.h
@@ -1,6 +1,8 @@
 #ifndef REPL_H
 #define REPL_H
 
+#include <cstring>
+
 #include "global.h"
 #include "parser.h"
 
@@ -8,8 +10,8 @@
  * @brief 강아지 아스키 아트 출력
  */
 void print_dog_ascii_art() {
-  printf("\n      / \\__\n");
-  printf("     (    @\\___\n");
+  printf("\n      /\\__\n");
+  printf("     (   @\\___\n");
   printf("    /         O   개소리 (DogSoundLang)\n");
   printf("   /   (_____/    version 0.1\n");
   printf("  /_____/   U\n\n");
@@ -22,20 +24,35 @@ void print_dog_ascii_art() {
 int repl() {
   print_dog_ascii_art();
 
-  char inputCmd[REPL_CMD_LENGTH] = "";
-
   while (true) {
     // Prompt
     printf("%s", REPL_PROMPT);
 
-    // 명령어 입력
-    if (!scanf("%99[^\n]%*c", inputCmd)) {
-      // 입력 없음
-      break;
+    char *cmd = NULL;
+    size_t len = 0;
+    ssize_t read;
+
+    read = getline(&cmd, &len, stdin);
+
+    if (cmd[0] == '\n') {
+      free(cmd);
+      continue;
+    }
+    
+    cmd[strlen(cmd) - 1] = '\0';
+    
+    if (strcmp(cmd, "exit") == 0) {
+      free(cmd);
+      printf("뫙뫙\n");
+      exit(0);
     }
 
     // 명령어 실행
-    parser(inputCmd, 1, "Main");
+    parser(cmd, 1, "Main");
+
+    free(cmd);
+    
+    printf("\n");
   }
 
   return 0;
